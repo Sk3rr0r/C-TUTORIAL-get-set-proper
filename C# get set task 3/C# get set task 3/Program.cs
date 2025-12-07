d438392b47009d0e6a6654d2032be9508be81747
Sk3rr0r <kryan700216@gmail.com>
2025-12-07 22:03:48 +0000

    i got lazy and this stuff just looks like c++ from here on

---
+++ C# get set task 3/C# get set task 3/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+bool CheckPrime(int userInt)
+{
+    for (int i = 2; i < userInt; i++)
+    {
+        if ((userInt % i) == 0) return false;
+    }
+    return true;
+}
+
+void FibSeq(int userInt)
+{
+    int temp = 0, temp2 = 1, temp3 = 0;
+    for (int i = 0; i < userInt; i++)
+    {
+        if ( i == 0 )
+        {
+            Console.Write("0, ");
+        }
+        if (i == 1)
+        {
+            Console.Write("1, ");
+        }
+        else
+        {
+            temp3 = temp + temp2;
+            Console.Write($"{temp3}, ");
+            temp = temp2;
+            temp2 = temp3;
+        }
+    }
+}
+
