817ee385b0cd164b7d85b74a675da9ad470db594
Sk3rr0r <kryan700216@gmail.com>
2025-12-07 21:34:52 +0000

    two done

---
+++ C # get set task 1/C # get set task 1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Hello, ");
+Console.WriteLine("[your name]!");
+Console.Write("You are ");
+Console.Write(2025 - 2007);
+Console.WriteLine(" years old");
+Console.WriteLine("");
+Console.WriteLine("On some walls you, if you press an object hard enough and push it up fast enough, then it will stick to the wall without falling.");
+Console.WriteLine("True");
+Console.WriteLine("");
+Console.WriteLine("Baby crocodiles develop their first set of teeth that they can carry for life very early in their lives, around 2 months after birth, and have only 2 extra sets to replace them if damaged");
+Console.WriteLine("False");
---
+++ C# get set task 2/C# get set task 2/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+float firstFlt, secondFlt;
+string userName, userNum;
+
+Console.Write("Enter first number: ");
+userNum = Console.ReadLine();
+firstFlt = float.Parse(userNum);
+
+Console.Write("Enter second number: ");
+userNum = Console.ReadLine();
+secondFlt = int.Parse(userNum);
+
+Console.WriteLine(firstFlt + secondFlt);
+
+
+Console.Write("Enter your name: ");
+userName = Console.ReadLine();
+
+Console.Write("Enter your birth year: ");
+userNum = Console.ReadLine();
+firstFlt = int.Parse(userNum);
+
+Console.WriteLine($"Hello, {userName}! You are {2025 - firstFlt} years old.");
+
+Console.Write("Enter the temperature in celsius: ");
+userNum = Console.ReadLine();
+firstFlt = int.Parse(userNum);
+Console.WriteLine($"{firstFlt} Celcius is {firstFlt + 273} Kelvin.");
+
+Console.WriteLine($"{firstFlt} Celcius is {(firstFlt * 9 / 5) + 32} Farenheit.");
+
+Console.Write("Enter first number: ");
+userNum = Console.ReadLine();
+firstFlt = int.Parse(userNum);
+
+Console.Write("Enter second number: ");
+userNum = Console.ReadLine();
+secondFlt = int.Parse(userNum);
+
+Console.WriteLine($"{firstFlt} + {secondFlt} = {firstFlt + secondFlt}");
+Console.WriteLine($"{firstFlt} - {secondFlt} = {firstFlt - secondFlt}");
+Console.WriteLine($"{firstFlt} * {secondFlt} = {firstFlt * secondFlt}");
+Console.WriteLine($"{firstFlt} / {secondFlt} = {firstFlt / secondFlt}");
